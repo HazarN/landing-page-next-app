af69dd8f9824538a3abe5e051644e1b3eda2f307
Hazar NAMDAR <hazarnamdarrr@gmail.com>
2023-12-21 03:32:56 +0300

    Added the hero section and also responsiveness for tablet and mobile devices

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
-import Image from 'next/image'
+import Hero from '@/components/Hero'
 
 export default function Home() {
   return (
-    <main className="font-bold">
-      Hello World
-    </main>
+    <>
+      <Hero />
+      
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+    </>
   )
 }
